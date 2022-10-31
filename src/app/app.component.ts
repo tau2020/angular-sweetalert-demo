53caec8e3bafd847e8c5f419562879cfc70c86ee
tau2020 <makombemt@gmail.com>
2022-10-31 12:26:18 +0200

    Angular sweetalert2 demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-sweetalert-demo';
+
+  constructor(){}
+  ngOnInit(): void {
+    console.log('life Cycle Hook with Spontaneous response');
+
+  }
+
+  tinyAlert(){
+    Swal.fire('Hey There!');
+
+  }
+
+  successNotification(){
+    Swal.fire('Hi', 'We have been informed!', 'success');
+  }
+
+  alertConfirmation(){
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'This process is irreversible.',
+      icon: 'warning',
+      showCancelButton:true,
+     confirmButtonText:'Yes, go ahead',
+     cancelButtonText:'No, let me think'
+
+    }).then((result)=>{
+      if(result.value){
+        Swal.fire('Removed', 'Product removed successfully', 'success');
+      }else if(result.dismiss === Swal.DismissReason.cancel){
+        Swal.fire('Camncelled', 'Product still in our database', 'error');
+      }
+    })
+  }
 }
